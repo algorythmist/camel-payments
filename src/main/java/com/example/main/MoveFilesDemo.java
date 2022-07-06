077c99314ada787ac06c2552aba8181045dc4d70
stochastician <dimitri@tecacet.com>
2022-07-06 03:33:00 +0000

    simple camel demo

--- src/main/java/com/example/main/MoveFilesDemo.java
+++ src/main/java/com/example/main/MoveFilesDemo.java
@@ -1,17 +1,31 @@
 package com.example.main;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+
+class FileProcessor implements Processor {
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+        System.out.println("Processing..." + exchange.getExchangeId());
+    }
+}
 
 class SimpleRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() {
-        from("file:input").to("file:output");
+        from("file:input")
+                .process(new FileProcessor())
+                .to("file:output");
     }
 
 }
+
 
 public class MoveFilesDemo {
 
